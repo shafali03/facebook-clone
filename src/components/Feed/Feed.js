f9e88d8678b59d036cbb56e2f31c11712cc2ce82
shafali03 <shaf03sm@gmail.com>
2020-09-17 17:51:31 +0100

    Created messageSender component and added styles

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Feed.css'
 import StoryReel from '../StoryReel/StoryReel'
+import MessageSender from '../MessageSender/MessageSender'
 
 function Feed() {
   return (
     <div className='feed'>
       <StoryReel />
+      <MessageSender />
     </div>
   )
 }
