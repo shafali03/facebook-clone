0aceda472de3b2d0e8f2c03aede0a2fe6b061dc4
shafali03 <shaf03sm@gmail.com>
2020-09-17 21:33:11 +0100

    Added post option icons for the post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Post.css'
 import { Avatar } from '@material-ui/core'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import NearMeIcon from '@material-ui/icons/NearMe'
+import { ExpandMoreOutlined } from '@material-ui/icons'
 
 function Post({ profilePic, image, username, timestamp, message }) {
   return (
@@ -21,6 +26,32 @@ function Post({ profilePic, image, username, timestamp, message }) {
       <div className="post__image">
         <img src={image} alt="" />
       </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlined />
+        </div>
+
+
+      </div>
+
+
     </div>
   )
 }
