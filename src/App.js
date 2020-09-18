ebb1da7b41e779804e4f3654419df61d5f283963
shafali03 <shaf03sm@gmail.com>
2020-09-18 20:02:42 +0100

    Created firebase authentication with react hook context API and reducer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
 import Login from './components/Login/Login';
+import { useStateValue } from './components/Context/StateProvider'
 
 function App() {
-  const user = null
+  const [{ user }, dispatch] = useStateValue()
 
   return (
     <div className="app">
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Login.css'
 import { Button } from '@material-ui/core'
+import { auth, provider } from '../../firebase'
+import { actionTypes } from '../Reducer/Reducer'
+import { useStateValue } from '../Context/StateProvider'
 
 function Login() {
+  const [state, dispatch] = useStateValue()
+
   const signIn = () => {
-
+    // sing in with google
+    auth.signInWithPopup(provider)
+      .then(result => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        })
+      })
+      .catch((error) => alert(error.message))
   }
-
 
   return (
     <div className='login'>
@@ -14,7 +26,7 @@ function Login() {
         <img src="https://res.cloudinary.com/shafali/image/upload/v1600440761/1200px-Facebook_Logo__2019_2_nxz8wo.png" alt="" />
         <img src="https://res.cloudinary.com/shafali/image/upload/v1600441119/Facebook-Logo.wine_1_snxrbd.png" alt="" />
       </div>
-      <Button type='submit' onClick>
+      <Button type='submit' onClick={signIn}>
         Sign In
       </Button>
     </div>
